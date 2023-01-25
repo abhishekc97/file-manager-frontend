b4e7c9359d6f4be9f0a446a89085231a4b300fef
Abhishek Choudhari <abhishekc97@gmail.com>
2023-01-26 01:28:46 +0530

    react boilderplate setup

---
+++ src/pages/FileExplorerHome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./FileExplorerHome.css"
+
+
+function FileExplorerHome() {
+    return ( 
+        <div className="file-explorer-home-container">
+            
+        </div>
+     );
+}
+
+export default FileExplorerHome;
